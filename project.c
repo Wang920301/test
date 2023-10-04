4dad4863ef0e48363de0e42a56c56a1f0b13d2f8
Wang920301 <eagle888168@gmail.com>
2023-10-04 16:48:44 +0800

    second change

--- project.c
+++ project.c
@@ -10,13 +10,13 @@ struct KeyValue database[MAX_KEYS];
 int numKeys = 0;
 void create(const char* key, const char* value) {
     if (numKeys < MAX_KEYS) {
-        strcpy_s(database[numKeys].key, key);
-        strcpy_s(database[numKeys].value, value);
+        strcpy(database[numKeys].key, key);
+        strcpy(database[numKeys].value, value);
         numKeys++;
-        printf("Key-Value pair created successfully.\n");
+        printf("created successfully.\n");
     }
     else {
-        printf("Database is full. Cannot create more pairs.\n");
+        printf("Cannot create more pairs.\n");
     }
 }
 void read(const char* key) {
@@ -31,19 +31,19 @@ void read(const char* key) {
 void update(const char* key, const char* value) {
     for (int i = 0; i < numKeys; i++) {
         if (strcmp(database[i].key, key) == 0) {
-            strcpy_s(database[i].value, value);
-            printf("Key-Value pair updated successfully.\n");
+            strcpy(database[i].value, value);
+            printf("updated successfully.\n");
             return;
         }
     }
-    printf("Key not found. Cannot update.\n");
+    printf("Key not found.\n");
 }
 void delet(const char* key) {
     for (int i = 0; i < numKeys; i++) {
         if (strcmp(database[i].key, key) == 0) {
             for (int j = i; j < numKeys - 1; j++) {
-                strcpy_s(database[j].key, database[j + 1].key);
-                strcpy_s(database[j].value, database[j + 1].value);
+                strcpy(database[j].key, database[j + 1].key);
+                strcpy(database[j].value, database[j + 1].value);
             }
             numKeys--;
             printf("Key-Value pair deleted successfully.\n");
@@ -59,30 +59,30 @@ int main() {
 
     while (1) {
         printf("Enter command (create/read/update/delete/quit): ");
-        scanf_s("%255s", &command, sizeof(command));
+        scanf("%s", command);
 
         if (strcmp(command, "create") == 0) {
             printf("Enter key: ");
-            scanf_s("%255s", &key, sizeof(key));
+            scanf("%s", key);
             printf("Enter value: ");
-            scanf_s("%255s", &value, sizeof(value));
+            scanf("%s", value);
             create(key, value);
         }
         else if (strcmp(command, "read") == 0) {
             printf("Enter key: ");
-            scanf_s("%255s", &key, sizeof(key));
+            scanf("%s", key);
             read(key);
         }
         else if (strcmp(command, "update") == 0) {
             printf("Enter key: ");
-            scanf_s("%255s", &key, sizeof(key));
+            scanf("%s", key);
             printf("Enter new value: ");
-            scanf_s("%255s", &value, sizeof(value));
+            scanf("%s", value);
             update(key, value);
         }
         else if (strcmp(command, "delete") == 0) {
             printf("Enter key: ");
-            scanf_s("%255s", &key, sizeof(key));
+            scanf("%s", key);
             delet(key);
         }
         else if (strcmp(command, "quit") == 0) {
